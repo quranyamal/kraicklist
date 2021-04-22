c4fd052c793556504f0530915d2de52d9988074b
quranyamal <mal.qurany@gmail.com>
2021-04-22 16:52:35 +0700

    insert search code into lambda function

--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module challenge.haraj.com.sa/kraicklist
 
 go 1.15
+
+require (
+	github.com/aws/aws-lambda-go v1.23.0 // indirect
+	github.com/russross/blackfriday/v2 v2.1.0 // indirect
+	github.com/urfave/cli/v2 v2.3.0 // indirect
+)
--- main.go
+++ main.go
@@ -22,8 +22,82 @@ type MyResponse struct {
 	Message string `json:"Result:"`
 }
 
+type Searcher struct {
+	records []Record
+}
+
+type Record struct {
+	ID        int64    `json:"id"`
+	Title     string   `json:"title"`
+	Content   string   `json:"content"`
+	ThumbURL  string   `json:"thumb_url"`
+	Tags      []string `json:"tags"`
+	UpdatedAt int64    `json:"updated_at"`
+	ImageURLs []string `json:"image_urls"`
+}
+
+func (s *Searcher) Load(filepath string) error {
+	// open file
+	file, err := os.Open(filepath)
+	if err != nil {
+		return fmt.Errorf("unable to open source file due: %v", err)
+	}
+	defer file.Close()
+	// read as gzip
+	reader, err := gzip.NewReader(file)
+	if err != nil {
+		return fmt.Errorf("unable to initialize gzip reader due: %v", err)
+	}
+	// read the reader using scanner to contstruct records
+	var records []Record
+	cs := bufio.NewScanner(reader)
+	for cs.Scan() {
+		var r Record
+		err = json.Unmarshal(cs.Bytes(), &r)
+		if err != nil {
+			continue
+		}
+		records = append(records, r)
+	}
+	s.records = records
+
+	return nil
+}
+
+func (s *Searcher) Search(query string) ([]Record, error) {
+	var result []Record
+	for _, record := range s.records {
+		if strings.Contains(record.Title, query) || strings.Contains(record.Content, query) {
+			result = append(result, record)
+		}
+	}
+	return result, nil
+}
+
 func HandleLambdaEvent(event MyEvent) (MyResponse, error) {
-	return MyResponse{Message: fmt.Sprintf("%s", event.Query)}, nil
+
+	// initialize searcher
+	searcher := &Searcher{}
+	err := searcher.Load("data.gz")
+	if err != nil {
+		log.Fatalf("unable to load search data due: %v", err)
+	}
+
+	// search relevant records
+	records, err := searcher.Search(event.Query)
+	if err != nil {
+		//w.WriteHeader(http.StatusInternalServerError)
+		//w.Write([]byte(err.Error()))
+		//return
+	}
+	// output success response
+	buf := new(bytes.Buffer)
+	encoder := json.NewEncoder(buf)
+	encoder.Encode(records)
+	//w.Header().Set("Content-Type", "application/json")
+	//w.Write(buf.Bytes())
+
+	return MyResponse{Message: fmt.Sprintf("%#v", records)}, nil
 }
 
 func main() {
@@ -79,56 +153,4 @@ func handleSearch(s *Searcher) http.HandlerFunc {
 			w.Write(buf.Bytes())
 		},
 	)
-}
-
-type Searcher struct {
-	records []Record
-}
-
-func (s *Searcher) Load(filepath string) error {
-	// open file
-	file, err := os.Open(filepath)
-	if err != nil {
-		return fmt.Errorf("unable to open source file due: %v", err)
-	}
-	defer file.Close()
-	// read as gzip
-	reader, err := gzip.NewReader(file)
-	if err != nil {
-		return fmt.Errorf("unable to initialize gzip reader due: %v", err)
-	}
-	// read the reader using scanner to contstruct records
-	var records []Record
-	cs := bufio.NewScanner(reader)
-	for cs.Scan() {
-		var r Record
-		err = json.Unmarshal(cs.Bytes(), &r)
-		if err != nil {
-			continue
-		}
-		records = append(records, r)
-	}
-	s.records = records
-
-	return nil
-}
-
-func (s *Searcher) Search(query string) ([]Record, error) {
-	var result []Record
-	for _, record := range s.records {
-		if strings.Contains(record.Title, query) || strings.Contains(record.Content, query) {
-			result = append(result, record)
-		}
-	}
-	return result, nil
-}
-
-type Record struct {
-	ID        int64    `json:"id"`
-	Title     string   `json:"title"`
-	Content   string   `json:"content"`
-	ThumbURL  string   `json:"thumb_url"`
-	Tags      []string `json:"tags"`
-	UpdatedAt int64    `json:"updated_at"`
-	ImageURLs []string `json:"image_urls"`
 }
