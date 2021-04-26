7e982b5aa86678162eb21d45b68c2c8cbf85e285
quranyamal <mal.qurany@gmail.com>
2021-04-27 05:12:53 +0700

    setup react app

---
+++ client/src/containers/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Home.css";
+
+export default function Home() {
+  return (
+    <div className="Home">
+      <div className="lander">
+        <h1>Kraicklist</h1>
+        <p className="text-muted">Haraj Take Home Challenge</p>
+      </div>
+    </div>
+  );
+}
