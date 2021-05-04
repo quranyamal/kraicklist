282dba68899da7779929bf3efc7cfe00bd35db4c
quranyamal <mal.qurany@gmail.com>
2021-05-04 11:45:20 +0700

    render image, tags and date

--- client/src/containers/Home.js
+++ client/src/containers/Home.js
@@ -5,6 +5,9 @@ import { useAppContext } from "../libs/contextLib";
 import { onError } from "../libs/errorLib";
 import { BsPencilSquare } from "react-icons/bs";
 import { LinkContainer } from "react-router-bootstrap";
+import { Col, Row } from "react-bootstrap";
+import Image from 'react-bootstrap/Image'
+import Badge from 'react-bootstrap/Badge'
 import "./Home.css";
 
 export default function Home() {
@@ -38,16 +41,33 @@ export default function Home() {
   function renderProductList(products) {
     return (
       <>
-        {products.map(({ id, title, content }) => (
+        {products.map(({ id, title, content, thumb_url, tags, updated_at }) => (
           <LinkContainer key={id} to={`/products/${id}`}>
             <ListGroup.Item action>
-              <span className="font-weight-bold">
-                {title}
-              </span>
-              <br />
-              <span className="text-muted">
-                {content}
-              </span>
+              <Row>
+                <Col xs={6} md={2}>
+                  <Image src={thumb_url} rounded />
+                </Col>
+                <Col xs={12} md={9}>
+                  <span className="font-weight-bold">
+                    {title}
+                  </span>
+                  <br />
+                  <span className="text-muted">
+                    {tags.map((tag) => (
+                        <span><Badge variant="secondary">{tag}</Badge> {' '}</span>
+                    ))}
+
+                  </span>
+                  <br />
+                  <span className="text-muted">
+                    Updated: {Intl.DateTimeFormat('en-US', 
+                      { year: 'numeric', month: '2-digit', day: '2-digit', hour: '2-digit', 
+                        minute: '2-digit', second: '2-digit' 
+                      }).format(updated_at*1000)}
+                  </span>
+                </Col>
+              </Row>
             </ListGroup.Item>
           </LinkContainer>
         ))}
